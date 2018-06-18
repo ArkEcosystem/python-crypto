25bed053c5598c4ed7da12d0e09c1d525789cc62
Tomaz Sifrer <tomazz.sifrer@gmail.com>
2018-06-18 22:46:43 +0200

    Added pytest integration and some other changes

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
+import sys
+
 import setuptools
 
-requires = [
 
+requires = [
 ]
 
 tests_require = [
     'flake8>=3.5.0',
     'flake8-import-order>=0.17.1',
+    'flake8-print>=3.1.0',
     'flake8-quotes>=1.0.0',
+    'pytest>=3.6.1',
 ]
 
 extras_require = {
     'test': tests_require,
     'dev': requires + tests_require
 }
+
+setup_requires = ['pytest-runner'] if {'pytest', 'test', 'ptr'}.intersection(sys.argv) else []
 
 setuptools.setup(
     name='ark-crypto',
@@ -22,10 +28,9 @@ setuptools.setup(
     author='',
     author_email='',
     url='https://github.com/ArkEcosystem/python-crypto',
-    packages=[
-        'ark',
-    ],
+    packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
     install_requires=requires,
     extras_require=extras_require,
     tests_require=tests_require,
+    setup_requires=setup_requires,
 )
