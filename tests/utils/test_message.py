deea0821f35bd8e66baaf9d090417d004ce9a5f4
Jolan <mc-somango@live.fr>
2018-12-18 09:18:01 +0100

    fix: messages now can work with both publickey and publicKey (#66)

--- tests/utils/test_message.py
+++ tests/utils/test_message.py
@@ -4,24 +4,38 @@ from crypto.utils.message import Message
 
 
 def test_signing(message):
-    result = Message.sign(message['data']['message'], message['passphrase'])
-    assert result.to_dict() == message['data']
+    result = Message.sign(message['camelCase_pk']['message'], message['passphrase'])
+    assert result.to_dict() == message['camelCase_pk']
 
 
-def test_verify(message):
-    result = Message(**message['data'])
+def test_verify_with_publickey(message):
+    result = Message(**message['snake_case_pk'])
+    assert result.verify() is True
+
+
+def test_verify_with_publicKey(message):
+    result = Message(**message['camelCase_pk'])
     assert result.verify() is True
 
 
 def test_to_dict(message):
-    result = Message(**message['data'])
-    assert result.to_dict() == message['data']
+    result = Message(**message['camelCase_pk'])
+    assert result.to_dict() == message['camelCase_pk']
 
 
-def test_to_json(message):
-    result = Message(**message['data'])
+def test_to_json_with_publicKey(message):
+    result = Message(**message['camelCase_pk'])
     json_data = result.to_json()
     data = json.loads(json_data)
-    assert data['signature'] == message['data']['signature']
-    assert data['public_key'] == message['data']['public_key']
-    assert data['message'] == message['data']['message']
+    assert data['signature'] == message['camelCase_pk']['signature']
+    assert data['publicKey'] == message['camelCase_pk']['publicKey']
+    assert data['message'] == message['camelCase_pk']['message']
+
+
+def test_to_json_with_publickey(message):
+    result = Message(**message['snake_case_pk'])
+    json_data = result.to_json()
+    data = json.loads(json_data)
+    assert data['signature'] == message['snake_case_pk']['signature']
+    assert data['publickey'] == message['snake_case_pk']['publickey']
+    assert data['message'] == message['snake_case_pk']['message']
