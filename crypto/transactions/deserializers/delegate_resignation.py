5c56fc24f2f4bc2dab55feafb1833cc959551f60
highjhacker <mc-somango@live.fr>
2019-11-14 19:00:12 +0100

    feat: delegate resignation deserialization now works

--- crypto/transactions/deserializers/delegate_resignation.py
+++ crypto/transactions/deserializers/delegate_resignation.py
@@ -1,6 +1,4 @@
-from binascii import hexlify, unhexlify
-
-from binary.unsigned_integer.reader import read_bit8
+from binascii import hexlify
 
 from crypto.transactions.deserializers.base import BaseDeserializer
 
@@ -8,18 +6,8 @@ from crypto.transactions.deserializers.base import BaseDeserializer
 class DelegateResignationDeserializer(BaseDeserializer):
 
     def deserialize(self):
-        starting_position = int(self.asset_offset / 2)
-
-        username_length = read_bit8(self.serialized, starting_position) & 0xff
-        start_index = self.asset_offset + 2
-        end_index = start_index + (username_length * 2)
-        username = hexlify(self.serialized)[start_index:end_index]
-        username = unhexlify(username)
-
-        self.transaction.asset['delegate'] = {'username': username.decode()}
-
         self.transaction.parse_signatures(
             hexlify(self.serialized),
-            self.asset_offset + (username_length + 1) * 2
+            self.asset_offset
         )
         return self.transaction
--- tests/transactions/deserializers/test_delegate_resignation.py
+++ tests/transactions/deserializers/test_delegate_resignation.py
@@ -2,8 +2,8 @@ from crypto.transactions.deserializer import Deserializer
 
 
 def test_delegate_resignation_deserializer():
-    serialized = 'ff02170100000007000000000000000000034151a3ec46b5670a682b0a63394f863587d1bc97483b1b6c70eb58e7f0aed19200f9029500000000000662fc939dbc79527786f4bfae360589d525e5c4e84bc0822eeccf9265601486445633813783ed166b37101b8faf595592557b75f84c842bc01221d64960c0e7'  # noqa
+    serialized = 'ff02170100000007000100000000000000034151a3ec46b5670a682b0a63394f863587d1bc97483b1b6c70eb58e7f0aed19200f902950000000000bdc048ca7eb5688cc01921aecf5914118cfc78eacc23825efa6d75094a683127cc02512dc59e1e0631fa8956f482eabc54933d23011a8337ea9cab99abed504d'  # noqa
     deserializer = Deserializer(serialized)
     actual = deserializer.deserialize()
-    assert actual.asset['type'] == 7
-    #actual.verify()
+    assert actual.type == 7
+    # actual.verify()
