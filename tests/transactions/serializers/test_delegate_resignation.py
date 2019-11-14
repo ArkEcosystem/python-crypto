730375dc4f236be72e34a10325101ad8d4e43722
highjhacker <mc-somango@live.fr>
2019-11-14 12:27:07 +0100

    feat: serialization for delegate resignation now works

--- tests/transactions/serializers/test_delegate_resignation.py
+++ tests/transactions/serializers/test_delegate_resignation.py
@@ -4,7 +4,5 @@ from crypto.transactions.serializer import Serializer
 
 
 def test_serializer(transaction_type_7):
-    print(transaction_type_7)
     result = Serializer(transaction_type_7).serialize(False, True)
-    print(result)
     assert result == transaction_type_7['serialized']
