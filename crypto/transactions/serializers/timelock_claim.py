dbf6651a3a49ba6119223fe1612ac70575651dc1
highjhacker <mc-somango@live.fr>
2019-11-14 17:12:36 +0100

    feat: timelock claim serialization now works

--- crypto/transactions/serializers/timelock_claim.py
+++ crypto/transactions/serializers/timelock_claim.py
@@ -1,9 +1,4 @@
-from binascii import hexlify
-
-from base58 import b58decode_check
-
-from binary.hex.writer import write_high, write_low
-from binary.unsigned_integer.writer import write_bit32, write_bit64
+from binascii import unhexlify, hexlify
 
 from crypto.transactions.serializers.base import BaseSerializer
 
@@ -13,9 +8,8 @@ class TimelockClaimSerializer(BaseSerializer):
     """
 
     def serialize(self):
-        self.bytes_data += write_bit64(self.transaction['recipientId']['amount'])
-        self.bytes_data += write_low(self.transaction['recipientId']['timelocktype'])
-        self.bytes_data += write_bit32(self.transaction['recipientId']['timelock'])
-        recipientId = hexlify(b58decode_check(self.transaction['recipientId']))
-        self.bytes_data += write_high(recipientId)
+        self.bytes_data += unhexlify(self.transaction['asset']['claim']['lockTransactionId'])
+        unlock_secret = hexlify(self.transaction['asset']['claim']['unlockSecret'].encode())
+        self.bytes_data += unhexlify(unlock_secret)
+
         return self.bytes_data
