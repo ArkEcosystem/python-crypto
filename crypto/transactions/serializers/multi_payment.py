28b84f16c10743ae9bdd76e9ad36f3f53224d5dc
highjhacker <mc-somango@live.fr>
2019-11-14 15:17:59 +0100

    feat : multi payments serialization now works

--- crypto/transactions/serializers/multi_payment.py
+++ crypto/transactions/serializers/multi_payment.py
@@ -1,21 +1,22 @@
-from binascii import hexlify
+from binascii import hexlify, unhexlify
 
-from base58 import b58decode_check
+from base58 import b58decode_check, b58encode_check
 
 from binary.hex.writer import write_high
-from binary.unsigned_integer.writer import write_bit32, write_bit64
+from binary.unsigned_integer.writer import write_bit16, write_bit64
 
 from crypto.transactions.serializers.base import BaseSerializer
-
 
 class MultiPaymentSerializer(BaseSerializer):
     """Serializer handling multi payment data
     """
 
     def serialize(self):
-        self.bytes_data += write_bit32(len(self.transaction['asset']['payments']))
+        self.bytes_data += write_bit16(len(self.transaction['asset']['payments']))
+
         for payment in self.transaction['asset']['payments']:
             self.bytes_data += write_bit64(payment['amount'])
-            recipientId = b58decode_check(hexlify(payment['recipientId']).encode())
+            recipientId = hexlify(b58decode_check(payment['recipientId']))
             self.bytes_data += write_high(recipientId)
+
         return self.bytes_data
