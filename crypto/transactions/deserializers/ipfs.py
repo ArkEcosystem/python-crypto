331707f2deb7ccc5bf512cf9711f2843ed1c4c5a
highjhacker <mc-somango@live.fr>
2019-11-15 21:40:21 +0100

    wip

--- crypto/transactions/deserializers/ipfs.py
+++ crypto/transactions/deserializers/ipfs.py
@@ -1,6 +1,7 @@
 from binascii import hexlify, unhexlify
 
 from binary.unsigned_integer.reader import read_bit8
+from base58 import b58encode_check
 
 from crypto.transactions.deserializers.base import BaseDeserializer
 
@@ -10,16 +11,26 @@ class IPFSDeserializer(BaseDeserializer):
     def deserialize(self):
         starting_position = int(self.asset_offset / 2)
 
-        username_length = read_bit8(self.serialized, starting_position) & 0xff
-        start_index = self.asset_offset + 2
-        end_index = start_index + (username_length * 2)
-        username = hexlify(self.serialized)[start_index:end_index]
-        username = unhexlify(username)
+        public_key = hexlify(self.serialized)[starting_position:starting_position + 66]
 
-        self.transaction.asset['delegate'] = {'username': username.decode()}
+        ipfs_hash_length = read_bit8(self.serialized, starting_position) & 0xff
+        start_index = self.asset_offset + 2
+        end_index = start_index + (ipfs_hash_length * 2)
+        ipfs_hash = hexlify(self.serialized)[start_index:end_index]
+        print(ipfs_hash)
+        ipfs_hash = b58encode_check(unhexlify(ipfs_hash)).decode() # Really not sure of that part
+        print(ipfs_hash)
+
+        self.transaction.asset = {
+            'ipfs': ipfs_hash
+        }
 
         self.transaction.parse_signatures(
             hexlify(self.serialized),
-            self.asset_offset + (username_length + 1) * 2
+            self.asset_offset + (ipfs_hash_length + 2) * 2
         )
+
+        print("SELF TRANSACTION")
+        print(self.transaction.to_dict())
+
         return self.transaction
--- tests/transactions/deserializers/test_ipfs.py
+++ tests/transactions/deserializers/test_ipfs.py
@@ -2,8 +2,9 @@ from crypto.transactions.deserializer import Deserializer
 
 
 def test_ipfs_deserializer():
-    serialized = 'ff02170100000005000000000000000000034151a3ec46b5670a682b0a63394f863587d1bc97483b1b6c70eb58e7f0aed1920065cd1d000000000012202853f0f11ab91d73b73a2a86606103f45dd469ad2e89ec6f9a25febe8758d3fe13109c588e5e2646756f13f4e73f8c0791c0ddf3508fbb34373c38817ce81e8c57ee09915fa5fd63487749ad91da2544795e0d8a1d1722a2fbfb94a58469c8fd'  # noqa
+    serialized = 'ff02170100000005000100000000000000034151a3ec46b5670a682b0a63394f863587d1bc97483b1b6c70eb58e7f0aed1920065cd1d000000000012202853f0f11ab91d73b73a2a86606103f45dd469ad2e89ec6f9a25febe8758d3fe0b6e81b123de99e953d3073a8760d3213ab5f5cf512e65a2dd73aebb410966d8fbc59e775deb4f23c51be0847402b5e1d4ee68732b3e6d8e8914d259d7e373eb'  # noqa
     deserializer = Deserializer(serialized)
     actual = deserializer.deserialize()
+    print(actual.__dict__)
     assert actual.asset['ipfs'] == 'QmR45FmbVVrixReBwJkhEKde2qwHYaQzGxu4ZoDeswuF9w'  # noqa
     # actual.verify()
