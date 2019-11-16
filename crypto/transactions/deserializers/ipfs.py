f288f9a3a3d622373e1fc78238d8fadda8b3cccc
highjhacker <mc-somango@live.fr>
2019-11-16 14:17:30 +0100

    feat: ipfs deserializer now works

--- crypto/transactions/deserializers/ipfs.py
+++ crypto/transactions/deserializers/ipfs.py
@@ -1,7 +1,9 @@
 from binascii import hexlify, unhexlify
 
 from binary.unsigned_integer.reader import read_bit8
-from base58 import b58encode_check
+from binary.unsigned_integer.writer import write_bit8
+
+from base58 import b58encode
 
 from crypto.transactions.deserializers.base import BaseDeserializer
 
@@ -11,26 +13,26 @@ class IPFSDeserializer(BaseDeserializer):
     def deserialize(self):
         starting_position = int(self.asset_offset / 2)
 
-        public_key = hexlify(self.serialized)[starting_position:starting_position + 66]
+        hash_function = read_bit8(self.serialized, starting_position) & 0xff
+        ipfs_hash_length = read_bit8(self.serialized, starting_position + 1) & 0xff
 
-        ipfs_hash_length = read_bit8(self.serialized, starting_position) & 0xff
-        start_index = self.asset_offset + 2
+        start_index = (starting_position + 2) * 2
         end_index = start_index + (ipfs_hash_length * 2)
         ipfs_hash = hexlify(self.serialized)[start_index:end_index]
-        print(ipfs_hash)
-        ipfs_hash = b58encode_check(unhexlify(ipfs_hash)).decode() # Really not sure of that part
-        print(ipfs_hash)
+
+        temp_buffer = bytes()
+
+        temp_buffer += write_bit8(hash_function)
+        temp_buffer += write_bit8(ipfs_hash_length)
+        temp_buffer += unhexlify(ipfs_hash)
 
         self.transaction.asset = {
-            'ipfs': ipfs_hash
+            'ipfs': b58encode(temp_buffer).decode()
         }
 
         self.transaction.parse_signatures(
             hexlify(self.serialized),
             self.asset_offset + (ipfs_hash_length + 2) * 2
         )
-
-        print("SELF TRANSACTION")
-        print(self.transaction.to_dict())
 
         return self.transaction
