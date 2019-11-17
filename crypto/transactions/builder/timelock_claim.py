386ccd64fd9565d2db3e61bc0417209d1d93ea32
highjhacker <mc-somango@live.fr>
2019-11-17 16:30:24 +0100

    feat: htlc lock builder implemented

--- crypto/transactions/builder/timelock_claim.py
+++ crypto/transactions/builder/timelock_claim.py
@@ -1,4 +1,4 @@
-from crypto.constants import TRANSACTION_TIMELOCK_CLAIM
+from crypto.constants import TRANSACTION_TIMELOCK_CLAIM, TRANSACTION_TYPE_GROUP
 from crypto.transactions.builder.base import BaseTransactionBuilder
 
 
@@ -6,12 +6,23 @@ class TimelockClaim(BaseTransactionBuilder):
 
     transaction_type = TRANSACTION_TIMELOCK_CLAIM
 
-    def __init__(self, fee=None):
+    def __init__(self, lock_transaction_id, unlock_secret, fee=None):
         """Create a timelock transaction
 
         Args:
             fee (int, optional): fee used for the transaction (default is already set)
         """
         super().__init__()
+
+        self.transaction.typeGroup = self.get_type_group()
+
+        self.transaction.asset['claim'] = {
+            'lockTransactionId': lock_transaction_id,
+            'unlockSecret': unlock_secret
+        }
+
         if fee:
             self.transaction.fee = fee
+
+    def get_type_group(self):
+        return TRANSACTION_TYPE_GROUP.CORE.value
--- tests/transactions/builder/test_timelock_claim.py
+++ tests/transactions/builder/test_timelock_claim.py
@@ -1,8 +1,22 @@
-import pytest
+from crypto.constants import TRANSACTION_TIMELOCK_CLAIM, TRANSACTION_TYPE_GROUP
+from crypto.transactions.builder.timelock_claim import TimelockClaim
 
-
-@pytest.mark.skip(reason='not implemented')
 def test_timelock_claim_transaction():
     """Test if timelock transaction gets built
     """
-    pass
+    lock_transaction_id = '943c220691e711c39c79d437ce185748a0018940e1a4144293af9d05627d2eb4'
+    # @TODO: Not sure here, shouldn't the builder take a bytes encoded string instead of accepting a plain string ?
+    unlock_secret = 'my secret that should be 32bytes'
+    transaction = TimelockClaim(lock_transaction_id, unlock_secret)
+    transaction.set_type_group(TRANSACTION_TYPE_GROUP.CORE)
+    transaction.set_nonce(123)
+    transaction.sign('testing')
+    transaction_dict = transaction.to_dict()
+    assert transaction_dict['nonce'] == 123
+    assert transaction_dict['signature']
+    assert transaction_dict['type'] is TRANSACTION_TIMELOCK_CLAIM
+    assert transaction_dict['typeGroup'] == 1
+    assert transaction_dict['typeGroup'] == TRANSACTION_TYPE_GROUP.CORE.value
+    assert transaction_dict['asset']['claim']['lockTransactionId'] == '943c220691e711c39c79d437ce185748a0018940e1a4144293af9d05627d2eb4'
+    assert transaction_dict['asset']['claim']['unlockSecret'] == 'my secret that should be 32bytes'
+    transaction.verify()  # if no exception is raised, it means the transaction is valid
