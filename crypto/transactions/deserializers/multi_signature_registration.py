5c5e609a40d6bc0877f8f987cd53bc34e600b0f6
highjhacker <mc-somango@live.fr>
2019-11-26 23:14:42 +0100

    feat: multi signature registration deserialization now works

--- crypto/transactions/deserializers/multi_signature_registration.py
+++ crypto/transactions/deserializers/multi_signature_registration.py
@@ -10,21 +10,41 @@ class MultiSignatureRegistrationDeserializer(BaseDeserializer):
     def deserialize(self):
         starting_position = int(self.asset_offset / 2)
 
-        self.transaction.asset = {
-            'multiSignature': {
-                'min': read_bit8(self.serialized, starting_position) & 0xff,
-                'lifetime': read_bit8(self.serialized, starting_position + 2) & 0xff,
-                'publicKeys': []
+        if self.transaction.version is None or self.transaction.version == 1:
+            self.transaction.asset = {
+                'multiSignatureLegacy': {
+                    'min': read_bit8(self.serialized, starting_position) & 0xff,
+                    'lifetime': read_bit8(self.serialized, starting_position + 2) & 0xff,
+                    'keysgroup': []
+                }
             }
-        }
 
-        count = read_bit8(self.serialized, starting_position + 3) & 0xff
-        for index in range(count):
-            index_start = int(self.asset_offset) + 6
-            if (index > 0):
-                index_start += index * 66
-            signature = hexlify(self.serialized)[index_start:index_start + 66].decode()
-            self.transaction.asset['multiSignature']['publicKeys'].append(signature)
+            keys_group_count = read_bit8(self.serialized, starting_position + 1) & 0xff
+
+            for index in range(keys_group_count):
+                index_start = int(self.asset_offset) + 4
+
+                if (index > 0):
+                    index_start += index * 66
+                signature = hexlify(self.serialized)[index_start:index_start + 66].decode()
+                self.transaction.asset['multiSignatureLegacy']['keysgroup'].append(signature)
+        else:
+            self.transaction.asset = {
+                'multiSignature': {
+                    'min': read_bit8(self.serialized, starting_position) & 0xff,
+                    'publicKeys': []
+                }
+            }
+
+            count = read_bit8(self.serialized, starting_position + 1) & 0xff
+
+            for index in range(count):
+                index_start = int(self.asset_offset) + 4
+
+                if (index > 0):
+                    index_start += index * 66
+                signature = hexlify(self.serialized)[index_start:index_start + 66].decode()
+                self.transaction.asset['multiSignature']['publicKeys'].append(signature)
 
         self.transaction.parse_signatures(
             hexlify(self.serialized),
