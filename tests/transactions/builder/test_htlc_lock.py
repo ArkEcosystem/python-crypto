5614b3530d5c7e4fa3e3e1ef96923ec8876671c7
Chris Johnson <chrisjohnsonmail@gmail.com>
2021-02-15 20:16:15 -0600

    feat: add amount to htlc lock transaction ctor
    Added 'amount' argument to the HTLC lock transation constructor.
    
    Added additional tests to cover new code.

--- tests/transactions/builder/test_htlc_lock.py
+++ tests/transactions/builder/test_htlc_lock.py
@@ -2,8 +2,48 @@ from crypto.configuration.network import set_network
 from crypto.constants import TRANSACTION_HTLC_LOCK, TRANSACTION_TYPE_GROUP
 from crypto.networks.devnet import Devnet
 from crypto.transactions.builder.htlc_lock import HtlcLock
+import pytest
 
 set_network(Devnet)
+
+
+def test_htlc_lock_transation_amount_not_int():
+    with pytest.raises(ValueError):
+        """Test error handling in constructor for non-integer amount
+        """
+        HtlcLock(
+          recipient_id='AGeYmgbg2LgGxRW2vNNJvQ88PknEJsYizC',
+          amount='bad amount number',
+          secret_hash='0f128d401958b1b30ad0d10406f47f9489321017b4614e6cb993fc63913c5454',
+          expiration_type=1,
+          expiration_value=1573455822
+        )
+
+
+def test_htlc_lock_transation_amount_zero():
+    with pytest.raises(ValueError):
+        """Test error handling in constructor for non-integer amount
+        """
+        HtlcLock(
+          recipient_id='AGeYmgbg2LgGxRW2vNNJvQ88PknEJsYizC',
+          amount=0,
+          secret_hash='0f128d401958b1b30ad0d10406f47f9489321017b4614e6cb993fc63913c5454',
+          expiration_type=1,
+          expiration_value=1573455822
+        )
+
+
+def test_htlc_lock_transation_amount_negative():
+    with pytest.raises(ValueError):
+        """Test error handling in constructor for non-integer amount
+        """
+        HtlcLock(
+          recipient_id='AGeYmgbg2LgGxRW2vNNJvQ88PknEJsYizC',
+          amount=-5,
+          secret_hash='0f128d401958b1b30ad0d10406f47f9489321017b4614e6cb993fc63913c5454',
+          expiration_type=1,
+          expiration_value=1573455822
+        )
 
 
 def test_htlc_lock_transaction():
@@ -11,6 +51,7 @@ def test_htlc_lock_transaction():
     """
     transaction = HtlcLock(
       recipient_id='AGeYmgbg2LgGxRW2vNNJvQ88PknEJsYizC',
+      amount=200000000,
       secret_hash='0f128d401958b1b30ad0d10406f47f9489321017b4614e6cb993fc63913c5454',
       expiration_type=1,
       expiration_value=1573455822
@@ -22,6 +63,7 @@ def test_htlc_lock_transaction():
     transaction_dict = transaction.to_dict()
 
     assert transaction_dict['recipientId'] == 'AGeYmgbg2LgGxRW2vNNJvQ88PknEJsYizC'
+    assert transaction_dict['amount'] == 200000000
     assert transaction_dict['nonce'] == 1
     assert transaction_dict['signature']
     assert transaction_dict['type'] is TRANSACTION_HTLC_LOCK
