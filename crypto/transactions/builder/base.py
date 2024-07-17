356a9eb9c0c0c2869c6be4dffd282e82f243ec0a
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-07-17 17:57:27 +0100

    transactions should be version 1

--- crypto/transactions/builder/base.py
+++ crypto/transactions/builder/base.py
@@ -19,7 +19,7 @@ class BaseTransactionBuilder(object):
         self.transaction.nonce = getattr(self, 'nonce', None)
         self.transaction.typeGroup = getattr(self, 'typeGroup', 1)
         self.transaction.signatures = getattr(self, 'signatures', None)
-        self.transaction.version = getattr(self, 'version', 2)
+        self.transaction.version = getattr(self, 'version', 1)
         if self.transaction.type != 0:
             self.transaction.amount = getattr(self, 'amount', 0)
 
