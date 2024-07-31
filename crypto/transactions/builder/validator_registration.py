d2e8bbd4c1979c558f4ebcc206261c73728b2ee5
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-07-31 12:16:50 +0100

    refactor: validate bls public key (#138)

--- crypto/transactions/builder/validator_registration.py
+++ crypto/transactions/builder/validator_registration.py
@@ -1,5 +1,7 @@
 from typing import Optional
 
+import blspy
+
 from crypto.constants import TRANSACTION_VALIDATOR_REGISTRATION
 from crypto.transactions.builder.base import BaseTransactionBuilder
 
@@ -15,7 +17,23 @@ class ValidatorRegistration(BaseTransactionBuilder):
         """
         super().__init__()
 
+        self.validate_bls_public_key(public_key)
+
         self.transaction.asset['validatorPublicKey'] = public_key
 
         if fee:
             self.transaction.fee = fee
+
+    def validate_bls_public_key(self, public_key: str):
+        """Validate BLS public key
+
+        Args:
+            public_key (str): BLS public key to validate
+        """
+        if len(public_key) != 96:
+            raise ValueError('Invalid BLS public key')
+
+        try:
+            blspy.PublicKeyMPL.from_bytes(bytes.fromhex(public_key))
+        except Exception:
+            raise ValueError('Invalid BLS public key')
