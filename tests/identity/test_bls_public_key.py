da41f86de3fe3d15c8df5bf666c38f03b1f4df44
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-08-12 17:00:12 +0100

    wip

--- tests/identity/test_bls_public_key.py
+++ tests/identity/test_bls_public_key.py
@@ -2,8 +2,42 @@ from crypto.identity.bls_public_key import BLSPublicKey
 
 
 def test_public_key_from_passphrase(validator):
-    public_key = BLSPublicKey.from_passphrase(validator['passphrase'])
-    assert public_key == validator['bls_public_key']
+    public_key = BLSPublicKey.from_passphrase_attempt_1(validator['passphrase'])
+
+    # assert public_key == validator['bls_public_key']
+    print('attempt_1', public_key, validator['bls_public_key'])
+
+
+
+    public_key = BLSPublicKey.from_passphrase_attempt_2(validator['passphrase'])
+
+    # assert public_key == validator['bls_public_key']
+    print('attempt_2', public_key, validator['bls_public_key'])
+
+
+
+    public_key = BLSPublicKey.from_passphrase_attempt_3(validator['passphrase'])
+
+    # assert public_key == validator['bls_public_key']
+    print('attempt_3', public_key, validator['bls_public_key'])
+
+
+
+    public_key = BLSPublicKey.from_passphrase_attempt_4(validator['passphrase'])
+
+    # assert public_key == validator['bls_public_key']
+    print('attempt_4', public_key, validator['bls_public_key'])
+
+
+
+    public_key = BLSPublicKey.from_passphrase_attempt_5(validator['passphrase'])
+
+    # assert public_key == validator['bls_public_key']
+    print('attempt_5', public_key, validator['bls_public_key'])
+
+
+
+    assert True == False
 
 
 # def test_public_key_from_hex(validator):
