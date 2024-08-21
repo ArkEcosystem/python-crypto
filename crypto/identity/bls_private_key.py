63e828a3d89c1ceffecade0151708df253991013
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-08-21 15:58:47 +0100

    fix: include submodule in packages (#142)

--- crypto/identity/bls_private_key.py
+++ crypto/identity/bls_private_key.py
@@ -4,7 +4,7 @@ import sys
 import os
 from os.path import dirname
 
-sys.path.append(os.path.join(dirname(dirname(dirname(__file__))), 'thirdparty/bls-signatures/python-impl'))
+sys.path.append(os.path.join(dirname(dirname(__file__)), 'thirdparty/bls-signatures/python-impl'))
 
 from schemes import BasicSchemeMPL
 
