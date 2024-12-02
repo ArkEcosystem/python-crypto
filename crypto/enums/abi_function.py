d5d9547f4615c2da46d3ea745c1fd16b60606241
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-12-02 09:18:35 -0600

    Update abi_function.py

--- crypto/enums/abi_function.py
+++ crypto/enums/abi_function.py
@@ -5,16 +5,3 @@ class AbiFunction(Enum):
     UNVOTE = 'unvote'
     VALIDATOR_REGISTRATION = 'registerValidator'
     VALIDATOR_RESIGNATION = 'resignValidator'
-
-    def transaction_class(self):
-        from crypto.transactions.types.vote import Vote
-        from crypto.transactions.types.unvote import Unvote
-        from crypto.transactions.types.validator_registration import ValidatorRegistration
-        from crypto.transactions.types.validator_resignation import ValidatorResignation
-
-        return {
-            AbiFunction.VOTE: Vote,
-            AbiFunction.UNVOTE: Unvote,
-            AbiFunction.VALIDATOR_REGISTRATION: ValidatorRegistration,
-            AbiFunction.VALIDATOR_RESIGNATION: ValidatorResignation
-        }[self]
