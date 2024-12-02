d6af57a2045496dc740e741f992f9b5abc26d311
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-12-02 08:53:05 -0600

    remove deprecated tests

--- tests/transactions/serializers/test_multi_signature_registration.py
+++
@@ -1,6 +0,0 @@
-from crypto.transactions.serializer import Serializer
-
-
-def test_serializer(transaction_type_4):
-    result = Serializer(transaction_type_4).serialize(False, True, False)
-    assert result == transaction_type_4['serialized']
--- tests/transactions/serializers/test_validator_resignation.py
+++
@@ -1,6 +0,0 @@
-from crypto.transactions.serializer import Serializer
-
-
-def test_serializer(transaction_type_7):
-    result = Serializer(transaction_type_7).serialize(False, True)
-    assert result == transaction_type_7['serialized']
