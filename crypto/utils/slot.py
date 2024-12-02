1e584a4b99f173916efcfeca1008aa28f352c3bd
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-12-02 09:17:46 -0600

    Update slot.py

--- crypto/utils/slot.py
+++ crypto/utils/slot.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone
+from datetime import datetime
 
 from crypto.configuration.network import get_network
 
@@ -9,7 +9,7 @@ def get_time():
     Returns:
         int: difference in seconds
     """
-    now = datetime.now(timezone.utc)
+    now = datetime.utcnow()
     network = get_network()
     seconds = int((now - network['epoch']).total_seconds())
     return seconds
