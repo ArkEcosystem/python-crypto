9b47db532bbad62274688b9500e726ab21116488
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-12-03 12:14:25 -0600

    remove deprecated serialzier/deserializer files

--- crypto/transactions/serializers/validator_resignation.py
+++
@@ -1,8 +0,0 @@
-from crypto.transactions.serializers.base import BaseSerializer
-
-class ValidatorResignationSerializer(BaseSerializer):
-    """Serializer handling validator resignation data
-    """
-
-    def serialize(self) -> bytes:
-        return self.bytes_data
