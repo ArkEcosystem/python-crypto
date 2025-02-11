b7cb1f239f5f1a03cd20c9a1c2553ba4a003ae25
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-02-11 20:51:03 +0000

    test

--- tests/identity/conftest.py
+++ tests/identity/conftest.py
@@ -16,6 +16,19 @@ def identity():
     return data
 
 @pytest.fixture
+def sign_compact():
+    """Identity fixture
+    """
+    data = {
+        'data': {
+            'serialized': '1f0567c4def813a66e03fa1cd499a27c6922698a67e25e0b38458d8f4bb0e581fc25fcdf9d110b82bde15bae4a49118deb83cfc3ec1656c2a29286d7836d328abe',
+            'message': 'ff13b004d11523dda1efac58723ed4c63a3afe61a5464498fcb5058de20aeb7a'
+        },
+        'passphrase': 'my super secret passphrase'
+    }
+    return data
+
+@pytest.fixture
 def validator():
     """Validator fixture
     """
--- tests/identity/test_private_key.py
+++ tests/identity/test_private_key.py
@@ -1,6 +1,25 @@
 from crypto.identity.private_key import PrivateKey
 
 
+
+from binascii import hexlify, unhexlify
+from hashlib import sha256
+
+from coincurve import PrivateKey as PvtKey
+from btclib.to_prv_key import PrvKey, int_from_prv_key, prv_keyinfo_from_prv_key, _prv_keyinfo_from_xprv
+from btclib.to_pub_key import pub_keyinfo_from_key
+from btclib.ecc import bms
+from btclib.b58 import p2pkh
+from btclib.ecc import dsa
+# from btclib.bip32 import BIP32KeyData
+from coincurve.ecdsa import der_to_cdata, serialize_compact, deserialize_compact
+
+from btclib.network import (
+    NETWORKS,
+)
+
+
+
 def test_private_key_from_passphrase(identity):
     private_key = PrivateKey.from_passphrase(identity['passphrase'])
     assert isinstance(private_key, PrivateKey)
@@ -11,3 +30,17 @@ def test_private_key_from_hex(identity):
     private_key = PrivateKey.from_hex(identity['data']['private_key'])
     assert isinstance(private_key, PrivateKey)
     assert private_key.to_hex() == identity['data']['private_key']
+
+def test_sign_compact(sign_compact):
+    private_key = PrivateKey.from_passphrase(sign_compact['passphrase'])
+
+    message = bytes.fromhex(sign_compact['data']['message'])
+    signature = private_key.sign_compact(message)
+
+    if isinstance(signature, str) or isinstance(signature, bytes):
+        serialized = signature
+    else:
+        serialized = signature.serialize()
+
+    # assert serialized[0] == 27
+    assert serialized.hex() == sign_compact['data']['serialized']
