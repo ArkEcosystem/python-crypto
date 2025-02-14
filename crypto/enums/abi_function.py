4d5f7408eec8af7a0aadedb6eb46976db10463d9
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-02-14 01:54:20 +0000

    feat: implement username transaction types

--- crypto/enums/abi_function.py
+++ crypto/enums/abi_function.py
@@ -3,5 +3,7 @@ from enum import Enum
 class AbiFunction(Enum):
     VOTE = 'vote'
     UNVOTE = 'unvote'
+    USERNAME_REGISTRATION = 'registerUsername'
+    USERNAME_RESIGNATION = 'resignUsername'
     VALIDATOR_REGISTRATION = 'registerValidator'
     VALIDATOR_RESIGNATION = 'resignValidator'
---
+++ crypto/transactions/types/username_registration.py
@@ -0,0 +1,21 @@
+from crypto.enums.contract_abi_type import ContractAbiType
+from crypto.transactions.types.abstract_transaction import AbstractTransaction
+from crypto.utils.abi_encoder import AbiEncoder
+from crypto.enums.abi_function import AbiFunction
+
+class UsernameRegistration(AbstractTransaction):
+    def __init__(self, data: dict = None):
+        data = data or {}
+        payload = self.decode_payload(data)
+        if payload:
+            data['username'] = payload.get('args', [None])[0] if payload.get('args') else None
+
+        super().__init__(data)
+
+    def get_payload(self) -> str:
+        if 'username' not in self.data:
+            return ''
+
+        encoder = AbiEncoder(ContractAbiType.USERNAMES)
+
+        return encoder.encode_function_call(AbiFunction.USERNAME_REGISTRATION.value, [self.data['username']])
