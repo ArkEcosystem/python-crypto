bcd7db03b4582152774c0b66449fba1a47906c0c
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-02-17 13:05:28 +0000

    refactor: set contract address in builder by default

--- crypto/transactions/builder/unvote_builder.py
+++ crypto/transactions/builder/unvote_builder.py
@@ -1,6 +1,13 @@
+from typing import Optional
+from crypto.enums.contract_addresses import ContractAddresses
 from crypto.transactions.builder.base import AbstractTransactionBuilder
 from crypto.transactions.types.unvote import Unvote
 
 class UnvoteBuilder(AbstractTransactionBuilder):
+    def __init__(self, data: Optional[dict] = None):
+        super().__init__(data)
+
+        self.recipient_address(ContractAddresses.CONSENSUS.value)
+
     def get_transaction_instance(self, data: dict):
         return Unvote(data)
--- crypto/transactions/builder/vote_builder.py
+++ crypto/transactions/builder/vote_builder.py
@@ -1,7 +1,14 @@
+from typing import Optional
+from crypto.enums.contract_addresses import ContractAddresses
 from crypto.transactions.builder.base import AbstractTransactionBuilder
 from crypto.transactions.types.vote import Vote
 
 class VoteBuilder(AbstractTransactionBuilder):
+    def __init__(self, data: Optional[dict] = None):
+        super().__init__(data)
+
+        self.recipient_address(ContractAddresses.CONSENSUS.value)
+
     def vote(self, vote: str):
         self.transaction.data['vote'] = vote
         self.transaction.refresh_payload_data()
