db8050a235405ed4da6ea8b751f2ed268642363e
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-02-17 13:05:34 +0000

    test

--- tests/transactions/builder/test_vote_builder.py
+++ tests/transactions/builder/test_vote_builder.py
@@ -27,3 +27,30 @@ def test_vote_transaction(passphrase, load_transaction_fixture):
     assert builder.transaction.serialize().hex() == fixture['serialized']
     assert builder.transaction.data['id'] == fixture['data']['id']
     assert builder.verify()
+
+def test_vote_transaction_with_default_recipient_address(passphrase, load_transaction_fixture):
+    fixture = load_transaction_fixture('vote')
+
+    builder = (
+        VoteBuilder()
+            .gas_price(fixture['data']['gasPrice'])
+            .nonce(fixture['data']['nonce'])
+            .network(fixture['data']['network'])
+            .gas_limit(fixture['data']['gasLimit'])
+            .vote('0xC3bBE9B1CeE1ff85Ad72b87414B0E9B7F2366763')  # Example vote address
+            .sign(passphrase)
+    )
+
+    assert builder.transaction.data['gasPrice'] == fixture['data']['gasPrice']
+    assert builder.transaction.data['nonce'] == fixture['data']['nonce']
+    assert builder.transaction.data['network'] == fixture['data']['network']
+    assert builder.transaction.data['gasLimit'] == fixture['data']['gasLimit']
+    assert builder.transaction.data['recipientAddress'].lower() == fixture['data']['recipientAddress'].lower()
+    assert builder.transaction.data['value'] == fixture['data']['value']
+    assert builder.transaction.data['v'] == fixture['data']['v']
+    assert builder.transaction.data['r'] == fixture['data']['r']
+    assert builder.transaction.data['s'] == fixture['data']['s']
+
+    assert builder.transaction.serialize().hex() == fixture['serialized']
+    assert builder.transaction.data['id'] == fixture['data']['id']
+    assert builder.verify()
