10c1f0c86ed53f34b62f0eeb0f333fc564cd90bc
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-02-21 01:52:58 +0000

    test validating addresses

---
+++ tests/utils/test_abi_base.py
@@ -0,0 +1,8 @@
+from crypto.utils.abi_base import AbiBase
+
+
+def test_it_should_validate_uppercase_addresses():
+    assert AbiBase().is_valid_address('0xC3bBE9B1CeE1ff85Ad72b87414B0E9B7F2366763') is True
+
+def test_it_should_validate_lowercase_addresses():
+    assert AbiBase().is_valid_address('0xc3bbe9b1cee1ff85ad72b87414b0e9b7f2366763') is True
