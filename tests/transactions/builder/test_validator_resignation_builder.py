3df7f1e1346c5a65f8dbf119374bfa8ae0946be1
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-02-21 11:39:42 +0000

    refactor: set contract address in builder by default (#150)

--- tests/transactions/builder/test_validator_resignation_builder.py
+++ tests/transactions/builder/test_validator_resignation_builder.py
@@ -26,3 +26,29 @@ def test_validator_resignation_transaction(passphrase, load_transaction_fixture)
     assert builder.transaction.serialize().hex() == fixture['serialized']
     assert builder.transaction.data['id'] == fixture['data']['id']
     assert builder.verify()
+
+def test_validator_resignation_transaction_with_default_recipient_address(passphrase, load_transaction_fixture):
+    fixture = load_transaction_fixture('validator-resignation')
+
+    builder = (
+        ValidatorResignationBuilder()
+            .gas_price(fixture['data']['gasPrice'])
+            .nonce(fixture['data']['nonce'])
+            .network(fixture['data']['network'])
+            .gas_limit(fixture['data']['gasLimit'])
+            .sign(passphrase)
+    )
+
+    assert builder.transaction.data['gasPrice'] == fixture['data']['gasPrice']
+    assert builder.transaction.data['nonce'] == fixture['data']['nonce']
+    assert builder.transaction.data['network'] == fixture['data']['network']
+    assert builder.transaction.data['gasLimit'] == fixture['data']['gasLimit']
+    assert builder.transaction.data['recipientAddress'].lower() == fixture['data']['recipientAddress'].lower()
+    assert builder.transaction.data['value'] == fixture['data']['value']
+    assert builder.transaction.data['v'] == fixture['data']['v']
+    assert builder.transaction.data['r'] == fixture['data']['r']
+    assert builder.transaction.data['s'] == fixture['data']['s']
+
+    assert builder.transaction.serialize().hex() == fixture['serialized']
+    assert builder.transaction.data['id'] == fixture['data']['id']
+    assert builder.verify()
