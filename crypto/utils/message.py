31a7e17ce6309776204475fbe9f92c2960a770d9
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-04-01 14:33:07 +0100

    fix some typing warnings in message class

--- crypto/utils/message.py
+++ crypto/utils/message.py
@@ -1,33 +1,34 @@
 import json
 from binascii import unhexlify
+from typing import Union
+
+from Cryptodome.Hash import keccak
 
 from crypto.identity.private_key import PrivateKey
-from crypto.transactions.signature import Signature
-from typing import Union
 
 class Message(object):
     public_key: bytes
     message: bytes
     signature: bytes
 
-    def __init__(self, public_key: bytes, message: bytes, signature: bytes):
-        if type(public_key) is bytes:
-            self.public_key = public_key
-        else:
+    def __init__(self, public_key: Union[bytes, str], message: Union[bytes, str], signature: Union[bytes, str]):
+        if isinstance(public_key, str):
             self.public_key = public_key.encode()
-
-        if type(message) is bytes:
-            self.message = message
         else:
+            self.public_key = public_key
+
+        if isinstance(message, str):
             self.message = message.encode()
-
-        if type(signature) is bytes:
-            self.signature = signature
         else:
+            self.message = message
+
+        if isinstance(signature, str):
             self.signature = signature.encode()
+        else:
+            self.signature = signature
 
     @classmethod
-    def sign(cls, message: Union[bytes, str], passphrase: bytes):
+    def sign(cls, message: Union[bytes, str], passphrase: Union[bytes, str]):
         """Signs a message
 
         Args:
