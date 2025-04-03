3659b90d67f0555bb1d5a2d80f0afdded6ed64ae
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-04-03 22:15:37 +0100

    refactor: class based slot module (#161)

--- crypto/utils/slot.py
+++ crypto/utils/slot.py
@@ -2,23 +2,24 @@ from datetime import datetime, timezone
 
 from crypto.configuration.network import Network
 
+class Slot:
+    @staticmethod
+    def time():
+        """Get the time difference between now and network start.
 
-def get_time() -> int:
-    """Get the time difference between now and network start.
+        Returns:
+            int: difference in seconds
+        """
+        now = datetime.now(timezone.utc)
 
-    Returns:
-        int: difference in seconds
-    """
-    now = datetime.now(timezone.utc)
+        seconds = int((now - Slot.epoch()).total_seconds())
 
-    seconds = int((now - get_epoch()).total_seconds())
+        return seconds
 
-    return seconds
+    @staticmethod
+    def epoch():
+        epoch_str = Network.get_network().epoch()
+        if epoch_str.endswith("Z"):
+            epoch_str = epoch_str[:-1] + "+00:00"
 
-
-def get_epoch():
-    epoch_str = Network.get_network().epoch()
-    if epoch_str.endswith("Z"):
-        epoch_str = epoch_str[:-1] + "+00:00"
-
-    return datetime.fromisoformat(epoch_str)
+        return datetime.fromisoformat(epoch_str)
