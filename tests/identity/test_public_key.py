9fc06d7d8261717ad31a76e6f5f5b31dc49853fe
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-04-14 22:32:28 +0100

    refactor: public key class

--- tests/identity/test_public_key.py
+++ tests/identity/test_public_key.py
@@ -1,16 +1,13 @@
-from binascii import hexlify
 from crypto.identity.public_key import PublicKey
 
 
 def test_public_key_from_passphrase(identity):
     public_key = PublicKey.from_passphrase(identity['passphrase'])
-    assert public_key == identity['data']['public_key']
+    assert public_key.public_key == identity['data']['public_key']
 
 
 def test_public_key_from_hex(identity):
     public_key = PublicKey.from_hex(identity['data']['public_key'])
     assert isinstance(public_key, PublicKey)
 
-    public_key_hex = hexlify(public_key.public_key.format()).decode()
-
-    assert public_key_hex == identity['data']['public_key']
+    assert public_key.public_key == identity['data']['public_key']
